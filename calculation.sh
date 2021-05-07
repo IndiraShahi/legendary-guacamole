c08d21de3bec1f6654c8c4f84e5fde6acb371d03
IndiraShahi <IndiraShahi144@gmail.com>
2021-05-07 12:11:16 +0530

    subtract fuction added to calculation.sh

--- calculation.sh
+++ calculation.sh
@@ -7,7 +7,15 @@ function add() {
          local c=$(( $a + $b ))
          echo $c
 }
+function sub() {
+         local a=$1
+         local b=$2
+         local c=$(( $a - $b ))
+         echo $c
+}
 a=15
 b=13
 result="$(add $a $b)"
+result1="$(sub $a $b)"
 echo "$result"
+echo "$result1"
